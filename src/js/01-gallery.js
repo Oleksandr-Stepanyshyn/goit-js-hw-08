690a30581c2c2dd0ca8738ff76e1339fa2fb13f7
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-20 22:41:20 +0200

    finish task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    galleryList: document.querySelector(".gallery"),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+console.log(galleryMarkup);
+refs.galleryList.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a></li>`
+}).join("");
+};
+
+var lightbox = new SimpleLightbox('.gallery li a', { captionsData: 'alt', captionDelay: 250 });
