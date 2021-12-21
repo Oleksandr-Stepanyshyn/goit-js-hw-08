2e4262b9d52a275a172a6872a3ea5c46a05108a8
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-21 14:09:34 +0200

    finish task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,18 +4,19 @@ var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
 
 const onPlay = function (currentTime) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
-    console.log(localStorage.getItem("videoplayer-current-time"));
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime.seconds));
+    console.log(localStorage.getItem(LOCALSTORAGE_KEY));
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {}).catch(function(error) {
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)).then(function (seconds) {}).catch(function(error) {
      switch (error.name) {
          case 'RangeError':
              break;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+
+const savedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+try {
+    form.elements.email.value = savedValue.email;
+    form.elements.message.value = savedValue.message;
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+    const formValue = {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValue));
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(savedValue);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.elements.email.value = "";
+    form.elements.message.value = "";
+}
