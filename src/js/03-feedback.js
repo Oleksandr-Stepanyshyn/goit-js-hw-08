c2d158cbedfdb35c1f96e5f30295cf5a3696e13c
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-23 15:09:14 +0200

    fixed all warnings

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,25 +2,15 @@ var throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = "feedback-form-state";
-
 const savedValue = parseStorage();
-try {
-    form.elements.email.value = savedValue.email;
-    form.elements.message.value = savedValue.message;
-} catch (error) {
-    console.log(error.name);
-    console.log(error.message);
-};
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-
-
 function onFormInput(e) {
     const formValue = {
-        email: form.elements.email.value,
-        message: form.elements.message.value
+        email: form.elements.email.value.trim(),
+        message: form.elements.message.value.trim()
     };
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValue));
     console.log(formValue);
@@ -28,15 +18,26 @@ function onFormInput(e) {
 
 function onFormSubmit(e) {
     e.preventDefault();
-    console.log(!form.elements.email.value.trim() && !form.elements.message.value.trim());
-    if (!form.elements.email.value.trim() && !form.elements.message.value.trim()) {
-        return;
+    if (!form.elements.email.value.trim() || !form.elements.message.value.trim()) {
+        return alert("Заполните все поля!!!");
     };
     const savedValue = parseStorage();
     console.log(savedValue);
     localStorage.removeItem(LOCALSTORAGE_KEY);
     form.reset();
 };
+
+function fillFormFromStorage() {
+    if (!savedValue) return;
+    try {
+    form.elements.email.value = savedValue.email;
+    form.elements.message.value = savedValue.message;
+        } catch (error) {
+            console.log(error.name);
+            console.log(error.message);
+           };
+};
+fillFormFromStorage();
 
 function parseStorage() {
    return JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
